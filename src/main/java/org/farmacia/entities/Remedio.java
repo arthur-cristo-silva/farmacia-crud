9d2bb2a023c654a965d323165bf48d484c4c5bdb
Thyago Henrique <165312819+Th14g0-Henri@users.noreply.github.com>
2024-06-06 18:08:51 -0300

    Update Remedio.java

--- src/main/java/org/farmacia/entities/Remedio.java
+++ src/main/java/org/farmacia/entities/Remedio.java
@@ -13,14 +13,18 @@ public class Remedio {
     private Uso uso;
     private String dosagem;
     private String descricao;
+    private int preço;
+    private int quantidade_estoque;
 
-    public Remedio(ArrayList<Substancia> substancias, String nome_remedio, LocalDate data_vencimento, int quantidade, float concentracao, Uso uso) {
+    public Remedio(ArrayList<Substancia> substancias, String nome_remedio, LocalDate data_vencimento, int quantidade, float concentracao, Uso uso, int preço,int quantidade_estoque) {
         this.substancias = substancias;
         this.nome_remedio = nome_remedio;
         this.data_vencimento = data_vencimento;
         this.quantidade = quantidade;
         this.concentracao = concentracao;
         this.uso = uso;
+        this.preço = preço;
+        this.quantidade_estoque = quantidade_estoque;
     }
 
     public ArrayList<Substancia> getSubstancias() {
@@ -86,5 +90,20 @@ public class Remedio {
     public void setDescricao(String descricao) {
         this.descricao = descricao;
     }
-}
 
+    public int  getPreço(){
+        return preço;
+    }
+
+    public void setPreço(int preço){
+        this.preço = preço;
+    }
+
+    public int getQuantidade_estoque(){
+        return quantidade_estoque;
+    }
+
+    public void setQuantidade_estoque(int quantidadeEstoque){
+        this.quantidade_estoque = quantidadeEstoque;
+    }
+}
